52f6576ee8684072c854f73a0e6ed431447a35dd
saritha <saritha.jce@gmail.com>
2022-10-26 02:09:40 +0530

    comments added

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from  pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s: ')
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s: ')
 
 package_name = "deepClassifier"
 
@@ -43,8 +43,8 @@ for filepath in list_of_files:
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,"w") as f:
-            pass 
-            logging.info(f'creating empty file: {filepath}')
+            pass #create an empty file
+            logging.info(f'Creating empty file: {filepath}')
 
     else:
         logging.info(f"{filename} already exists")
